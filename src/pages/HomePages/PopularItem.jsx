73bece9090e888a893288761530fd29757bba315
HasnaAkter <hasnaakter.cse@gmail.com>
2024-01-06 15:50:12 +0600

    ll

--- src/pages/HomePages/PopularItem.jsx
+++ src/pages/HomePages/PopularItem.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const PopularItem = () => {
@@ -55,7 +54,7 @@ const PopularItem = () => {
   return (
     <div className="p-10 px-24 max-w-screen-xl mx-auto">
       <div className="flex justify-between">
-        <h3 className="text-2xl font- text-start py-3">Popular</h3>
+        <h3 className="text-2xl font- text-start py-3">Recommended</h3>
         <p className="text-start py-3">
           <button
             className="btn text-orange-500"
@@ -63,28 +62,27 @@ const PopularItem = () => {
           >
             Add More
           </button>
-
-          <dialog id="my_modal_1" className="modal ">
-            <div className="modal-box ">
-              {items.map((item, index) => (
-                <div
-                  key={item.Id}
-                  className={`flex items-center p-2 ${
-                    selectedItems.includes(index) ? "text-orange-500" : ""
-                  }`}
-                  onClick={() => toggleSelectedItem(index)}
-                >
-                  <img
-                    src={item.ImageUrl}
-                    alt={item.Name}
-                    style={{ height: "50px", width: "50px" }}
-                    className="rounded-[25px]"
-                  />
-                  <p className="card-title text-[14px] text-center ml-2">
-                    {item.Name}
-                  </p>
-                </div>
-              ))}
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              {items.map((item) => (
+                  <div
+                    key={item.Id}
+                    className={`flex items-center p-2 ${
+                      selectedItems.includes(item.Id) ? "text-orange-500" : ""
+                    }`}
+                    onClick={() => toggleSelectedItem(item.Id)}
+                  >
+                    <img
+                      src={item.ImageUrl}
+                      alt={item.Name}
+                      style={{ height: "50px", width: "50px" }}
+                      className="rounded-[25px]"
+                    />
+                    <p className=" card-title text-[14px] text-center ml-2">
+                      {item.Name}
+                    </p>
+                  </div>
+                ))}
 
               <div className="modal-action">
                 <form method="dialog">
@@ -108,7 +106,6 @@ const PopularItem = () => {
               </div>
             </div>
           </dialog>
-
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -148,40 +145,36 @@ const PopularItem = () => {
         className="mySwiper"
       >
         {items.map((item) => (
-          <SwiperSlide key={item.Id}>
+            <SwiperSlide key={item.Id}>
+              <div className="flex flex-col items-center">
+                <img
+                  src={item.ImageUrl}
+                  alt={item.Name}
+                  style={{ height: "250px", width: "500px" }}
+                  className="rounded-[25px]"
+                />
+                <p className="card-title text-[14px] text-center mt-2">
+                  {item.Name}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+
+        {selectedItemsForSwiper.map((selectedItem) => (
+          <SwiperSlide key={selectedItem.Id}>
             <div className="flex flex-col items-center">
               <img
-                src={item.ImageUrl}
-                alt={item.Name}
+                src={selectedItem.ImageUrl}
+                alt={selectedItem.Name}
                 style={{ height: "250px", width: "500px" }}
                 className="rounded-[25px]"
               />
               <p className="card-title text-[14px] text-center mt-2">
-                {item.Name}
+                {selectedItem.Name}
               </p>
             </div>
           </SwiperSlide>
         ))}
-
-       {selectedItemsForSwiper.map((selectedItem) => (
-  <SwiperSlide key={selectedItem.Id}>
-    <div className="flex flex-col items-center">
-      {selectedItem && selectedItem.ImageUrl && (
-        <img
-          src={selectedItem.ImageUrl}
-          alt={selectedItem.Name}
-          style={{ height: "250px", width: "500px" }}
-          className="rounded-[25px]"
-        />
-      )}
-      {selectedItem && selectedItem.Name && (
-        <p className="card-title text-[14px] text-center mt-2">
-          {selectedItem.Name}
-        </p>
-      )}
-    </div>
-  </SwiperSlide>
-))}
       </Swiper>
     </div>
   );
